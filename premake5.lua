2e7553220aad5849bee538a5a0eab01ded559db7
lukas <ezyn69@gmail.com>
2023-06-17 12:08:57 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,5 @@
---GLFW project
+
+-- GLFW
 
 project "GLFW"
 	kind "StaticLib"
@@ -27,29 +28,6 @@ project "GLFW"
 		"src/vulkan.c",
 		"src/window.c",
 	}
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
 
 	filter "system:windows"
 		systemversion "latest"
@@ -72,6 +50,29 @@ project "GLFW"
 		{ 
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
+		}
+	
+	filter "system:linux"
+		systemversion "latest"
+		pic "On"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
 		}
 
 	filter "configurations:Debug"
